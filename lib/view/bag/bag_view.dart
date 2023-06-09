fe0b486aba8bc909de850744501ab8c2f585d5a4
FauziHidayat <hidayat.fauzi52@gmail.com>
2023-06-09 08:46:54 +0700

    Update 9 Juni

---
+++ lib/view/bag/bag_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BagView extends StatefulWidget {
+  const BagView({Key? key}) : super(key: key);
+
+  @override
+  State<BagView> createState() => _BagViewState();
+}
+
+class _BagViewState extends State<BagView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
